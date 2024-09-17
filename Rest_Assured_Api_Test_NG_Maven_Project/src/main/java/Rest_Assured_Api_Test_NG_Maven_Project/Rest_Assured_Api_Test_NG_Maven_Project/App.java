e678d83ba1c17f1362f5470b26f6601a601c94f2
Anowar Hossain <157941809+Anowar-Git24@users.noreply.github.com>
2024-09-17 00:17:12 -0400

    Add files via upload
    Initial Commit Api Test

---
+++ Rest_Assured_Api_Test_NG_Maven_Project/src/main/java/Rest_Assured_Api_Test_NG_Maven_Project/Rest_Assured_Api_Test_NG_Maven_Project/App.java
@@ -0,0 +1,10 @@
+package Rest_Assured_Api_Test_NG_Maven_Project.Rest_Assured_Api_Test_NG_Maven_Project;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
